3ac96fd68989752a4dbc842df535d197e405d64b
Lachovski <145782520+LachovskiJohnnie@users.noreply.github.com>
2025-07-01 08:21:08 -0300

    h

--- main.js
+++ main.js
@@ -38,11 +38,11 @@ function calculaTempo(tempoObjetivo){
 
 if(tempoFinal > 0){
     return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
-else{
+}else{
     return "Prazo Finalizado."
 }
 
-}
+
 }
 
 function atualizaCronometro(){
